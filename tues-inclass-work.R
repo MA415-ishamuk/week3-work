a295dec088b448aabf1ca4e0e6c46f88309c0921
ishamuk <ishamuk@bu.edu>
2025-02-04 15:10:18 -0500

    done initial work to read in buoy data table from 1985 and begin doing preprocessing

---
+++ tues-inclass-work.R
@@ -0,0 +1,34 @@
+# notes from class
+# r defaults to characters - you can recast as numbers
+# use unique() to find how many unique values occur in a given column of a table
+  # ex: unique(buoy_data_1985$DPD) --> gives 99 (only value in the column is 99)
+# use <- NA to convert all values of a column in a table to NA 
+
+###
+# initial buoy project
+###
+
+# read in table
+# between the file_root and tail is the year of the data that is wanted
+# (this way you can easily loop through and read in each year's data)
+file_root <- "https://www.ndbc.noaa.gov/view_text_file.php?filename=44013h" 
+tail <- ".txt.gz&dir=data/historical/stdmet/"
+
+# function to easily generate url to access buoy data per year 
+# (connects file_root and tail via a particular year)
+load_buoy_data <- function(year)
+  path <- paste0(file_root, year, tail)
+
+path_1985 <- load_buoy_data("1985") # create the data path to access data from 1985
+
+#read in information as a table into R
+buoy_data_1985 <- read.table(path_1985, fill = T, header = T, sep = "") 
+
+
+# data preprocessing
+# initialize columns with incorrect values to NA
+buoy_data_1985$WVHT <- NA
+
+# check to make sure all of the data in this column is incorrect before replacing
+unique(buoy_data_1985$DPD)
+buoy_data_1985$DPD <- NA
